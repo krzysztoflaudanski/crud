dd798f99192e2af2320d4d135346bd6ba1732f6b
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-08-08 16:59:44 +0200

    add-navbar

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -8,10 +8,13 @@ const NavBar = () => {
     return (
         <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
             <Container>
-                <Navbar.Brand href="#home">Blog.app</Navbar.Brand>
-                <Nav className="me-auto, justify-content">
-                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-                    <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                <Navbar.Brand href="/">Blog.app</Navbar.Brand>
+                <Nav>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu"><span className="navbar-toggler-icon"></span></button>
+                    <div className="collapse navbar-collapse" id="mainMenu">
+                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                    </div>
                 </Nav>
             </Container>
         </Navbar>

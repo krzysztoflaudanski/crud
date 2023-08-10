64d6dcbdacbd70a7a2850399cf0d5c2039b30cc4
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-08-10 13:14:33 +0200

    add function to add post

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -1,27 +1,55 @@
 import { Button, Form } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { useState } from 'react';
+import { addPost } from "../../../redux/postsRedux";
+import { useNavigate } from "react-router-dom";
 
 const AddPostForm = () => {
+
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [publishedDate, setPublishedDate] = useState('');
+    const [shortDescription, setShortDescription] = useState('');
+    const [content, setContent] = useState('');
+
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addPost({ title, author, publishedDate, shortDescription, content }))
+        setTitle('');
+        setAuthor('');
+        setPublishedDate('')
+        setShortDescription('')
+        setContent('')
+        navigate('/')
+    }
+
+
     return (
-        <Form className="">
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{width:'300px'}}>
+        <Form onSubmit={handleSubmit} className="">
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{ width: '300px' }}>
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="Enter title"/>
+                <Form.Control type="text" placeholder="Enter title" value={title} onChange={e => setTitle(e.target.value)} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{width:'300px'}}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{ width: '300px' }}>
                 <Form.Label>Author</Form.Label>
-                <Form.Control type="text" placeholder="Enter author"/>
+                <Form.Control type="text" placeholder="Enter author" value={author} onChange={e => setAuthor(e.target.value)} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{width:'300px'}}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" style={{ width: '300px' }}>
                 <Form.Label>Published</Form.Label>
-                <Form.Control type="text" placeholder="Enter author"/>
+                <Form.Control type="text" placeholder="Enter author" value={publishedDate} onChange={e => setPublishedDate(e.target.value)} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" style={{minWidth:'300px'}}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" style={{ minWidth: '300px' }}>
                 <Form.Label>Short description</Form.Label>
-                <Form.Control as="textarea" rows={3} placeholder="Leave a comment here"/>
+                <Form.Control as="textarea" rows={3} placeholder="Leave a comment here" value={shortDescription} onChange={e => setShortDescription(e.target.value)} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" style={{minWidth:'300px'}}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" style={{ minWidth: '300px' }}>
                 <Form.Label>Main content</Form.Label>
-                <Form.Control as="textarea" rows={10} placeholder="Leave a comment here"/>
+                <Form.Control as="textarea" rows={10} placeholder="Leave a comment here" value={content} onChange={e => setContent(e.target.value)} />
             </Form.Group>
             <Button type="submit">Add Post</Button>
         </Form>
